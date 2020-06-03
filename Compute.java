2982719273ffd8fa194c1796e97386fd060b1969
PrismaticAxis <PrismaticAxis@gmail.com>
2020-06-03 14:57:41 -0600

    Upload exercise 1-5

---
+++ Compute.java
@@ -0,0 +1,14 @@
+/*
+ Author: Stephen Lovell
+ Date: 03 Jun 2020
+ 
+ A simple program that outputs the result
+ of a specific algebraic expression
+ */
+public class Compute 
+{
+	public static void main(String[] args)
+	{
+		System.out.println( (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5) );
+	}
+}
