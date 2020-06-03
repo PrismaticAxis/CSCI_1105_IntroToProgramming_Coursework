bec836277e5f7e40fb5dc811e86b42aedcb64c24
PrismaticAxis <PrismaticAxis@gmail.com>
2020-06-03 15:07:14 -0600

    Upload exercise 1-9 (AreaPerimeter)

---
+++ Chapter_01/AreaPerimeter.java
@@ -0,0 +1,17 @@
+/*
+ Author:	Stephen Lovell
+ Date:		03 Jun 2020
+ 
+ Outputs the area and perimeter of a rectangle with
+ specific dimensions.
+ */
+public class AreaPerimeter
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Area: ");
+		System.out.println(4.5 * 7.9);
+		System.out.print("Perimeter: ");
+		System.out.println(2 *(4.5) + 2 * (7.9));
+	}
+}
